6e04919e19a34476dc376c4a778e814867a5fc79
TedW-g <tedw3@nycstudents.net>
2024-09-19 13:27:42 +0000

    commit

--- Feeder.java
+++ Feeder.java
@@ -25,6 +25,13 @@ public class Feeder {
 
     public void simulateOneDay(int numBirds)
     {
+        if(Math.random() < 0.05) currentFood = 0;
+        else
+        {
+            int eaten = (int)(Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if(currentFood < 0) currentFood = 0;
+        }
     }
 
     /**
--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ public class Main
     {
         Feeder g = new Feeder(500);
         System.out.println(g.getCurrentFood());
-        double random = (int) (Math.random() * 10 + 1);
+        double random = (int) (Math.random() * 41 + 10);
         System.out.println(random);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
     }
 
 }
